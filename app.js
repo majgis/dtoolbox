eab0cf1a8a5c12179d2f153e778946c476d7d1d1
Michael Jackson <mjackson@overstock.com>
2015-11-08 15:24:17 -0700

    The rendering engine was switched to handlebars.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var cookieParser = require('cookie-parser');
-
+var handlebars = require('express-handlebars');
 var githubAuth = require('./server/githubAuth');
 
 var secrets = require('./server/secrets.json');
@@ -9,8 +9,15 @@ var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URL || secrets.mongodb.mongoUrl);
 
 app.use(express.static(__dirname + '/public'))
-    .use('/login', githubAuth)
-    .use(cookieParser());
+		.use('/login', githubAuth)
+		.use(cookieParser())
+		.engine('handlebars', handlebars())
+		.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+	console.log('token:', req);
+	res.render('index');
+});
 
 var port = 8080;
 app.listen(port);
