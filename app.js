38c342ab8eef4bdd9d139cb238aa9cc517706255
Michael Jackson <mjackson@overstock.com>
2015-11-08 15:45:03 -0700

    The user name and avatar is now shown if logged in.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var cookieParser = require('cookie-parser');
 var handlebars = require('express-handlebars');
 var githubAuth = require('./server/githubAuth');
-
+var User = require('./server/models/User');
 var secrets = require('./server/secrets.json');
 var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URL || secrets.mongodb.mongoUrl);
@@ -15,8 +15,29 @@ app.use(express.static(__dirname + '/public'))
 		.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
-	console.log('token:', req);
-	res.render('index');
+	var token = req.cookies.token;
+	if (token){
+		User.find({uid: token}, function (err, users) {
+			if (err) throw err;
+			var user;
+			if (users.length > 0) {
+				user = users[0];
+				console.log('user found:', user.userId);
+
+				res.render('index', {
+					isAuthenticated: true,
+					userId: user.userId,
+					avatarUrl: user.avatar_url
+				});
+			} else {
+				res.render('index');
+			}
+		});
+	} else {
+		res.render('index', {test:true});
+	}
+
+
 });
 
 var port = 8080;
