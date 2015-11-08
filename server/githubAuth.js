f743f43ac5410fff188ae1e0c591bb5fcc45f46d
Michael Jackson <mjackson@overstock.com>
2015-11-08 11:02:49 -0700

    Now retrieving github access token

--- server/githubAuth.js
+++ server/githubAuth.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var uuid = require('node-uuid');
 var secrets = require('./secrets.json');
+var request = require('request');
 
 //TODO: Move to database before scaling
 var states = {};
@@ -20,17 +21,40 @@ app
 		//Redirect back from github
 		.get('/github/authorize', function (req, res) {
 
-			var githubCode = req.query.code;
-			var state = states[req.query.state];
-			if (state){
-				delete states[req.query.state];
+			var stateKey = req.query.state;
+			var stateValue = states[stateKey];
+
+			if (stateValue){
+				delete states[stateKey];
 				console.log('The state was recognized.');
-				res.status(200);
+
+				var githubCode = req.query.code;
+
+				request({
+					url: "https://github.com/login/oauth/access_token",
+					method: "POST",
+					json: true,
+					body: {
+						client_id: secrets.github.clientId,
+						client_secret: secrets.github.clientSecret,
+						code: githubCode,
+						state: stateKey
+					}
+				}, function (error, response, body){
+					if (error){
+						console.log("There was an issue getting the github access token:", error);
+						res.redirect('/');
+					} else {
+						console.log('body', body);
+						res.append('Authentication', body.access_token);
+						res.redirect('/');
+					}
+				});
+
 			} else {
-				console.log('SECURITY: Unknown github auth state:', req.query.state);
-				res.status(401);
+				console.log('SECURITY: Unknown github auth state:', stateKey);
+				res.status(401).end();
 			}
-			res.end();
 		});
 
 module.exports = app;
